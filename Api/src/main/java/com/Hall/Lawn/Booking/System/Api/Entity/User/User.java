b626df7dfe247f228bbde800ce6a93ec1e83793d
Saud Khan <143578711+saud9422@users.noreply.github.com>
2026-06-13 19:00:31 +0530

    the change are done in user entity

--- Api/src/main/java/com/Hall/Lawn/Booking/System/Api/Entity/User/User.java
+++ Api/src/main/java/com/Hall/Lawn/Booking/System/Api/Entity/User/User.java
@@ -2,15 +2,24 @@ package com.Hall.Lawn.Booking.System.Api.Entity.User;
 
 import com.Hall.Lawn.Booking.System.Api.Enums.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.security.Identity;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name ;
     private String email ;
     private String password ;
@@ -18,8 +27,9 @@ public class User {
     private String phone;
     private Role role;
     private boolean isActive;
-    private Data createdOn;
-    private Data updatedOn;
+    private Date createdOn;
+    private Date updatedOn;
+
 
 
 }
